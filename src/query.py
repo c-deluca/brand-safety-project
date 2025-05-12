6f23c9fcb5f2d9008a661479f4573b0e3d9efa2b
Chiara De Luca <c.deluca@gedidigital.it>
2025-05-12 16:58:05 +0200

    upload

--- src/query.py
+++ src/query.py
@@ -3,8 +3,8 @@ SELECT url -- id, article_id, tipo, testata, title, url, testo, creation_timesta
 FROM gd-gcp-prd-dp-lake-2-0.gd_gcp_prd_dp_lake_2_bq_0.cms_cms_atex_tutti_contenutieditoriali_snp 
 where tipo = "video"
 and testata = "repubblica"
-and category in ("esteri")
-and creation_timestamp between ("2025-04-01 00:00:00 UTC") and "2025-04-05 00:00:00 UTC"
+and category in ("esteri", "cronaca")
+and creation_timestamp between ("2025-04-11 00:00:00 UTC") and "2025-05-11 00:00:00 UTC"
 order by creation_timestamp desc
-limit 3
+limit 100
 """
