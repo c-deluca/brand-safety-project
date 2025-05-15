c40512b6cb731ef97d382a7c164562b95a6e1715
Chiara De Luca <c.deluca@gedidigital.it>
2025-05-15 12:28:17 +0200

    upload

--- src/main.py
+++ src/main.py
@@ -20,11 +20,11 @@ def main():
     df = json_to_dataframe(cleaned_output)
 
 
-    with open("output/output_december.json", "w", encoding="utf-8") as f:
+    with open("output/output_dec_new.json", "w", encoding="utf-8") as f:
         json.dump(cleaned_output, f, ensure_ascii=False, indent=4)
 
 
-    df.to_csv('output/output_december.csv', index=False)
+    df.to_csv('output/output_dec_new.csv', index=False)
 
     return df
     
--- src/query.py
+++ src/query.py
@@ -3,7 +3,7 @@ FROM gd-gcp-prd-dp-lake-2-0.gd_gcp_prd_dp_lake_2_bq_0.cms_cms_atex_tutti_contenu
 where tipo = "video"
 and testata = "repubblica"
 and category in ("esteri", "cronaca")
---and creation_timestamp between ("2024-12-01 00:00:00 UTC") and "2025-01-01 00:00:00 UTC"
-order by creation_timestamp desc
-limit 200
+and creation_timestamp between ("2024-12-01 00:00:00 UTC") and "2025-01-01 00:00:00 UTC"
+order by creation_timestamp 
+limit 20
 """
