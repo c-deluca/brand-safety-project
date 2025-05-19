8b0910a70447c4fa23f6ad2a1b8a3dae575d4e1e
Chiara De Luca <c.deluca@gedidigital.it>
2025-05-19 18:06:29 +0200

    upload

--- src/query.py
+++ src/query.py
@@ -3,9 +3,10 @@ FROM gd-gcp-prd-dp-lake-2-0.gd_gcp_prd_dp_lake_2_bq_0.cms_cms_atex_tutti_contenu
 where tipo = "video"
 and testata = "repubblica"
 and category in ("esteri", "cronaca")
---and creation_timestamp between ("2020-12-01 00:00:00 UTC") and "2025-01-01 00:00:00 UTC"
---and url like any ('%camilli%', '%parisi_del_sismi%')
-and url like any ('%gdf_novara%', '%a_cuba_sfila%', '%nave_italiana%') --'%gli_avvocati_di_via_poma%') --'%parisi_del_sismi%', '%spari_nella_notte_a_perugia%')
+and creation_timestamp between ("2024-12-01 00:00:00 UTC") and "2025-01-01 00:00:00 UTC"
+--and url like any ('%contraff%')
+--and url like any ('%cuba_sfila%') --'%gdf_novara%', '%a_cuba_sfila%', '%nave_italiana%', '%gli_avvocati_di_via_poma%') --'%parisi_del_sismi%', '%spari_nella_notte_a_perugia%')
+--and url like any ('%mar_nero_caccia%') --'%guerra%', '%ebrei%', '%pace%', '%sostanz%', '%sess%', '%alcol%')
 --order by creation_timestamp 
-limit 10
+limit 15
 """
