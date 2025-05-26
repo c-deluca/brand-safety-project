0397b48a2e17ff22a2afbb14ca52a668147b3d67
Chiara De Luca <c.deluca@gedidigital.it>
2025-05-26 10:12:16 +0200

    upload

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from utils import generate_for_videos, clean_json_output, json_to_dataframe
 import extraction
-import json
+import json, pandas as pd
 from extraction import extraction_func, video_extr
 from conf import generation_config, system_instruction, safety_settings
 
@@ -10,11 +10,12 @@ with open("input/prompt.txt", "r", encoding="utf-8") as f:
 
 def main():
 
-    # video_urls = [#" https://media.gedidigital.it/repubblicatv/file/2025/04/09/1096403/1096403-video-rrtv-650-usa_come_non_impugnare_un_fucile.mp4",
-    #           "https://media.gedidigital.it/repubblicatv/file/2025/04/09/1096339/1096339-video-rrtv-650-talk2.mp4"]
+    #video_urls = [" https://media.gedidigital.it/repubblicatv/file/2025/04/09/1096403/1096403-video-rrtv-650-usa_come_non_impugnare_un_fucile.mp4","https://media.gedidigital.it/repubblicatv/file/2025/04/09/1096339/1096339-video-rrtv-650-talk2.mp4"]
     
-    video_urls = video_extr()
-    
+    #video_urls = video_extr()
+    df_dec = pd.read_csv("url_dicembre.csv")
+    video_urls = df_dec["url_tvmanager"].dropna().tolist()[:3]
+
     output = generate_for_videos(video_urls, system_instruction, prompt, generation_config, safety_settings)
     cleaned_output = clean_json_output(output)
     df = json_to_dataframe(cleaned_output)
