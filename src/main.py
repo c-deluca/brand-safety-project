02f2a0ea30720aef2ef7cc354cc39178d74474be
Chiara De Luca <c.deluca@gedidigital.it>
2025-06-04 12:04:15 +0200

    update - reorganize project structure

--- src/main.py
+++ src/main.py
@@ -12,20 +12,20 @@ def main():
 
     #video_urls = [" https://media.gedidigital.it/repubblicatv/file/2025/04/09/1096403/1096403-video-rrtv-650-usa_come_non_impugnare_un_fucile.mp4","https://media.gedidigital.it/repubblicatv/file/2025/04/09/1096339/1096339-video-rrtv-650-talk2.mp4"]
     
-    #video_urls = video_extr()
-    df_dec = pd.read_csv("url_dicembre.csv")
-    video_urls = df_dec["url_tvmanager"].dropna().tolist()[:100]
+    #video_urls = video_extr() -- per ricavare link tvmanager da html
+    df_dec = pd.read_csv("data/url_dicembre.csv") # -- link dicembre '24 per analisi preliminare
+    video_urls = df_dec["url_tvmanager"].dropna().tolist()[8:10]
 
     output = generate_for_videos_prova(video_urls, system_instruction, prompt, generation_config, safety_settings)
     cleaned_output = clean_json_output(output)
     df = json_to_dataframe(cleaned_output)
 
 
-    with open("output_trials.json", "w", encoding="utf-8") as f:
+    with open("output/output.json", "w", encoding="utf-8") as f:
         json.dump(cleaned_output, f, ensure_ascii=False, indent=4)
 
 
-    df.to_csv('output_trials.csv', index=False)
+    df.to_csv('output/output.csv', index=False)
 
     return df
     
